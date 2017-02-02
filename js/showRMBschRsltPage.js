8f5f7b69edd0ef18677a41956947703cdee09f12
Qiguang <qiguang138457@gmail.com>
2017-02-02 10:48:10 +0800

    fixed a bug of showing RMB prices
    when the original prices is a range, like $12-$23,
    the RMB convertion didn't work properly.

--- js/showRMBschRsltPage.js
+++ js/showRMBschRsltPage.js
@@ -1,24 +1,51 @@
 function convert2RMB(orig, exchangeRate) {
-	return orig*exchangeRate;
+	console.log("convert2RMB");
+	var nums = new Array;
+	if (orig) {
+		for (var i=0; i< orig.length; ++i) {
+			nums[i] = Number(orig[i]*exchangeRate);
+			console.log(nums[i]);
+		}
+	}
+	return nums;
 }
 // input: "$ 123 45" output: "123.45"
 function formatOrig(orig) {
-	return orig.replace(RegExp("\\D*([0-9]+)\\s+([0-9]+)"), "$1.$2");
+	console.log("formatOrig");
+	var strings = new Array;
+	strings = orig.match(RegExp("\\D*[0-9]+\\s+[0-9]+",'g'));
+	if (strings) {
+		for (var i=0; i<strings.length; ++i) {
+			strings[i]=strings[i].replace(RegExp("\\D*([0-9]+)\\s+([0-9]+)"), "$1.$2");
+			console.log(strings[i]);
+		}
+	}
+	return strings;
 }
 function showRMB(exchangeRate, style) {
 	console.log("showRMB");
 	var elements = document.getElementsByClassName("sx-price sx-price-large");
 	if (elements && elements.length != 0) {
-		var RMB;
 		for (var i=0; i < elements.length; ++i) {
-			RMB = convert2RMB(formatOrig(data_of(elements[i])),exchangeRate);
+			var RMB = convert2RMB(formatOrig(data_of(elements[i])),exchangeRate);
+			if (!RMB) continue;
 			console.log("RMB="+RMB);
 			var node = elements[i].cloneNode(true);
 			node.setAttribute("style",style);
 			node.setAttribute("name","RMB");
-			node.getElementsByClassName("sx-price-currency")[0].innerHTML="RMB";
-			node.getElementsByClassName("sx-price-whole")[0].innerHTML=RMB.toFixed(0);
-			node.removeChild(node.getElementsByClassName("sx-price-fractional")[0]);
+			var childs = node.getElementsByClassName("sx-price-currency");
+			for (var x=0; x<childs.length; ++x) {
+				childs[x].innerHTML="RMB";
+			}
+			childs = node.getElementsByClassName("sx-price-whole");
+			for (var x=0; x<childs.length; ++x) {
+				childs[x].innerHTML=RMB[x].toFixed(0);
+			}
+			childs = node.getElementsByClassName("sx-price-fractional");
+			for (var x=0; x<childs.length; ++x) {
+				node.removeChild(childs[x]);
+				--x;
+			}
 			elements[i].appendChild(node);
 			++i;
 		}
